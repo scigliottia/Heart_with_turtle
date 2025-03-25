ae9807872311587340c10436fe299454d09b8b26
scigliottia <scigliottia@gmail.com>
2025-03-25 05:36:48 -0300

    Corregi una linea de Adivinanza.py

--- Adivinanza.py
+++ Adivinanza.py
@@ -4,7 +4,6 @@ def adivina_el_numero():
     numero_secreto = random.randint(1, 100)
     intentos = 0
 
-    print("¡Bienvenido al juego de adivinanza de números!")
     print("Estoy pensando en un número entre 1 y 100.")
 
     while True:
